6e2e8fd298ec2920346d2aebf5aad91e8a98a2c9
Cristina Emin <chira.cristina10@gmail.com>
2022-11-11 14:26:39 +0200

    product page

--- src/NarBar.js
+++ src/NarBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavLink } from 'react-router-dom';
 import styled from "styled-components";
 import { useQuery, gql } from "@apollo/client";
@@ -10,40 +10,6 @@ const CATEGORIES_QUERY = gql`
       }   
 }
 `;
-
-const NavBar = () => {
-  
-    const { data, loading, error } = useQuery(CATEGORIES_QUERY);
-
-    if (loading) return "Loading...";
-    if (error) return <pre>{error.message}</pre>
-    
-    return(
-   <Navigation> 
-    <ul className="navigation">
-        {/* <li>
-            <NavLink to="/women" activeClassName="active" className="label"><p className="label-text">Women</p></NavLink>
-        </li>
-        <li>
-            <NavLink to="/men" activeClassName="active" className="label"><p className="label-text">Men</p></NavLink>
-        </li>
-        <li>
-            <NavLink to="/kids" activeClassName="active" className="label"><p className="label-text">Kids</p></NavLink>
-        </li> */}
-
-        {data.categories.map((category) => (
-          <li key={category.id}>
-             <NavLink to={`/${category.name}`} activeClassName="active" className="label"><p className="label-text">{category.name}</p></NavLink>
-            </li>
-        ))}
-
-    </ul>
-        
-    </Navigation>
-
-);
-}
-
 
 const Navigation = styled.nav`
     /* Navigation */
@@ -59,4 +25,37 @@ const Navigation = styled.nav`
     bottom: 0px;
 `;
 
+
+const NavBar = () => {
+  
+  
+
+    const { data, loading, error } = useQuery(CATEGORIES_QUERY);
+
+    if (loading) return "Loading...";
+    if (error) return <pre>{error.message}</pre>
+
+
+   return(
+    <Navigation> 
+        <ul className="navigation">
+        
+            {data.categories.map((category) => (
+            <li key={category.id}>
+                <NavLink to={`/${category.name}`} activeClassName="active" className="label"><p className="label-text">{category.name}</p></NavLink>
+                </li>
+            ))}
+
+        </ul>
+    </Navigation>
+)
+        
+  
+   
+   
+}
+
+
+
 export default NavBar;
+

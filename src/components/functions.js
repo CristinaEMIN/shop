9e9ac285f5716f90cf5f49949291c61b6316038a
Cristina Emin <chira.cristina10@gmail.com>
2023-01-27 17:16:14 +0200

    addToCart-conditions, cartTotal

--- src/components/functions.js
+++ src/components/functions.js
@@ -36,3 +36,32 @@ export function compareObjectExeceptKey(object1, object2, exceptKeys){
 
     return false;
 }
+
+export function compareObjects(obj1, obj2){
+   
+    if(!isObject(obj1) || !isObject(obj2)){
+        return false;
+    }
+    let len = null;
+    //check if they're of thesame length
+    if(Object.keys(obj1).length != Object.keys(obj2).length){
+        return false;
+    }else{
+        len = Object.keys(obj1).length; //use any length
+    }
+
+    let match = 0; //store number of matched properties
+    Object.keys(obj1).forEach(i => {
+        //check if values with the same keys are equal
+        if(obj1[i] === obj2[i]){
+        match++; //increment the variable
+    } }
+        )
+    //check if object length equals the number of matched properties
+    if(match === len){
+        return true;
+    }
+
+    return false;
+}
+
